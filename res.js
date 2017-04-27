9f5b75ee003eaf4fca0c4cc98be418870f760bbe
nawartamawi <nawartamawi@gmail.com>
2017-04-27 04:29:13 +0000

    WIP

---
+++ res.js
@@ -0,0 +1,5 @@
+module.exports = { <?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say voice="woman">Please leave a message after the tone.</Say>
+    <Record maxLength="20" />
+</Response> }
