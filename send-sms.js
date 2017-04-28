2dd9c67fe63dbb5e9f4dd890ee14d790f7d728e9
nawartamawi <nawartamawi@gmail.com>
2017-04-28 00:22:50 +0000

    ADSD

--- send-sms.js
+++ send-sms.js
@@ -2,20 +2,20 @@
 var accountSid = 'ACe9eca1e952d162dbb2da90738c4e2490';
 var authToken = 'e9f218c1c679644751ebf96ad16caa04';
  
-//require the Twilio module and create a REST client
+// //require the Twilio module and create a REST client
 var client = require('twilio')(accountSid, authToken);
- 
+//  var mesg = "HEllp" ;
 // client.messages.create({
 //     to: "+17782324505",
 //     from: "+17786519742",
-//     body: "You have an order blah blah blah ",
+//     body: mesg,
 // }, function(err, message) {
 //     console.log(message.sid);
 // });
 
 client.calls.create({
   method: 'GET',
-  url: "https://murmuring-river-48866.herokuapp.com/order.xml",
+  url: "http://397cb175.ngrok.io/order2.xml",
   to: "+17782324505",
   from: "+17786519742"
 }, function(err, call) {
