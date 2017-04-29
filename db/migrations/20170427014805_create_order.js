cda0f6fa304c662cb66ef9cc83b18176d9bc1638
Kevin Morissette <morissette.kevin@gmail.com>
2017-04-29 23:55:04 +0000

    update

--- db/migrations/20170427014805_create_order.js
+++ db/migrations/20170427014805_create_order.js
@@ -3,7 +3,9 @@
 exports.up = function(knex) {
   return knex.schema.createTable("orders", (table) => {
     table.increments();
-    table.bigInteger('client_id').unsigned().index().references('id').inTable('clients')
+    // table.bigInteger('client_id').unsigned().index().references('id').inTable('clients');
+    table.integer('client_id').unsigned();
+    table.foreign('client_id').references('clients.id');
   })
 };
 
