c61a993771e240701f9ab7ddf2c60f7b8d3b7a61
Elvis Chan <choectt@gmail.com>
2017-04-30 05:08:44 +0000

    Bug fixes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,6 @@
 // appending dishes data, render order sidebar to home page
 $(() => {
+  $(this).scrollTop(0);
   const currentOrder = {};
   // const sms = require('send-sms').sendSMS;
 
@@ -8,7 +9,7 @@ $(() => {
   }
 
   function dishTemplate(dish) {
-    return `<section class='col-xs-6 col-sm-4'>
+    return `<section class='col-xs-12 col-sm-4'>
               <div class='dish' data-dishid='${dish.id}'>
                 <div class='dish-img-wrapper'><img class='dish-img' src='${dish.img_url}'></div>
                 <div class='caption'>
@@ -54,7 +55,7 @@ $(() => {
                 <input class='form-control' type='tel' id='phone_number' name='phone_number' placeholder='(555) 555-5555'>
               </div>
               <div>Total: $${total}</div>
-              <input class='btn btn-primary btn-lg btn-block pay-order' type='submit' role='button' value='Pay'>
+              <input class='btn btn-primary btn-lg btn-block pay-order' type='submit' role='button' value='Place Order'>
             </form>`;
   }
 
@@ -64,14 +65,13 @@ $(() => {
       const currObj = currentOrder[prop];
       total += currObj.price * currObj.quantity;
     }
-    // console.log(total);
     return total;
-  };
+  }
 
   function paintPage(res) {
     $('.menu-wrapper').append(res.map(dishTemplate));
 
-    $('.fa-minus-square-o').on('click', function() {
+    $('.fa-minus-square-o').on('click', function () {
       const $that = $(this);
       const $counter = $that.siblings('.counter');
       const $menuContainer = $that.closest('[data-dishid]');
@@ -86,18 +86,15 @@ $(() => {
           if ($currentVal > 0) {
             const newVal = $currentVal - 1;
             $counter.text(newVal);
-            let $dishInCart = $cartContainer.find('[data-dishid="' + dishIDfromMenu + '"]');
+            const $dishInCart = $cartContainer.find(`[data-dishid="${  dishIDfromMenu  }"]`);
             const currentQuantity = (!$dishInCart) ? 0 : currentOrder[dishIDfromMenu].quantity;
 
             if (currentQuantity > 1) {
               $dishInCart.find('.counter').text(` X ${newVal}`);
             } else if (currentQuantity === 1) {
-              $('.place-order').attr('disabled', 'disabled');
               $dishInCart.remove();
             }
             currentOrder[dishIDfromMenu].quantity--;
-          } else {
-            $that.addClass('inactive');
           }
           const total = calculateTotal();
           $('#cart-total').text(total);
@@ -106,7 +103,7 @@ $(() => {
         });
     });
 
-    $('.fa-plus-square').on('click', function() {
+    $('.fa-plus-square').on('click', function () {
       const $that = $(this);
       const $counter = $that.siblings('.counter');
       const $menuContainer = $that.closest('[data-dishid]');
@@ -116,6 +113,7 @@ $(() => {
       const $cartContainer = $('.selected-dish');
 
       $('.place-order').removeAttr('disabled');
+      
       ajaxCall('PUT', '/orders')
         .then(() => {
           const $currentVal = +$counter.text();
@@ -129,17 +127,25 @@ $(() => {
           };
           currentOrder[dishIDfromMenu] = item;
 
-          const $dishInCart = $cartContainer.find('[data-dishid="' + dishIDfromMenu + '"]');
+          const $dishInCart = $cartContainer.find(`[data-dishid="${ dishIDfromMenu }"]`);
           if ($dishInCart.length) {
-            $dishInCart.find('.counter').text(' X ' + newVal);
+            $dishInCart.find('.counter').text(` X ${newVal}`);
           } else {
             $cartContainer.append(cartTemplate(item));
           }
-          const total = calculateTotal();
-          $('#cart-total').text(total);
+          $('#cart-total').text(calculateTotal());
         }, (err) => {
           console.error('we have a problem!!!');
         });
+
+        // // remove single item in cart
+        // $('.fa-remove').on('click', function () {
+        //   const $dishInCart = $cartContainer.find('[data-dishid="' + dishIDfromMenu + '"]');
+        //   $dishInCart.remove();
+        //   $counter.text('0');
+        //   $('#cart-total').text(calculateTotal());
+        // });
+
     });
   }
 
@@ -168,10 +174,10 @@ $(() => {
   $(window).on('scroll', () => {
     const header = $('header');
     const range = 200;
-    let scrollTop = $(this).scrollTop();
+    const scrollTop = $(this).scrollTop();
     let offset = header.offset();
-    let height = header.outerHeight();
-    offset = offset + height / 2;
+    const height = header.outerHeight();
+    offset += height / 2;
     const calc = 1 - (scrollTop - offset + range) / range;
 
     header.css({
@@ -193,6 +199,23 @@ $(() => {
   $('.btn-down').click(() => {
     $('html,body').animate({
       scrollTop: $('#menu').offset().top }, 'slow');
+  });
+
+   // affix cart
+  const $attribute = $('[data-smart-affix]');
+  $attribute.each(function () {
+     $(this).affix({
+       offset: {
+         top: $(this).offset().top + 100,
+         right: $(this).offset().right,
+       },
+     });
+  });
+
+  $(window).on('resize', () => {
+     $attribute.each(function () {
+       $(this).data('bs.affix').options.offset = $(this).offset().top;
+     });
   });
 });
 
