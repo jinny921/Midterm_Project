d660cf674063e50cf57adb63ea965076bc91ca72
Jinny Huang <jinny921@gmail.com>
2017-06-22 21:49:26 +0000

    edited images and read-me

--- db/seeds/restaurant_dishes.js
+++ db/seeds/restaurant_dishes.js
@@ -10,12 +10,12 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       const insertDataPromise = Promise.all([
         knex('restaurants').insert({name: 'Maenam', address: '217 E Georgia Street', phone_number: '604-569-8192'}),
-        knex('dishes').insert({name: 'Pad Thai', preptime: 45, img_url: 'http://i.imgsafe.org/25a5bf1c7c.jpg', price: 16, description: 'fresh rice noodles, tamarind, peanuts, tofu, egg, prawns'}),
-        knex('dishes').insert({name: 'Massaman Curry of Braised Beef', preptime: 50, img_url: 'http://i.imgsafe.org/3b09797416.jpeg', price: 22, description: 'roasted peanut, potato confit, longan, roasted onion'}),
-        knex('dishes').insert({name: 'Chicken Cashew Stirfry', preptime: 45, img_url: 'http://i.imgsafe.org/25a2b2c3f1.jpg', price: 19, description: 'fresh baby corn, green peppercorn, thai basil'}),
+        knex('dishes').insert({name: 'Pad Thai', preptime: 45, img_url: 'http://www.cbc.ca/inthekitchen/assets_c/2013/01/PadThai25-thumb-596x350-258207.jpg', price: 16, description: 'fresh rice noodles, tamarind, peanuts, tofu, egg, prawns'}),
+        knex('dishes').insert({name: 'Massaman Curry of Braised Beef', preptime: 50, img_url: 'http://img.taste.com.au/YLyVbGax/taste/2016/11/slow-cooker-massaman-beef-curry-85846-1.jpeg', price: 22, description: 'roasted peanut, potato confit, longan, roasted onion'}),
+        knex('dishes').insert({name: 'Chicken Cashew Stirfry', preptime: 45, img_url: 'https://www.rowsehoney.co.uk/rowse2017/wp-content/uploads/2015/10/58-Chicken-and-cashew-Stir-Fry@2x.jpg', price: 19, description: 'fresh baby corn, green peppercorn, thai basil'}),
         knex('dishes').insert({name: 'Green Papaya Salad', preptime: 30, img_url: 'https://www.maangchi.com/wp-content/uploads/2011/12/mangosomtam1.jpg', price: 12, description: "long beans, carrots, garlic, roasted peanuts, bird's eye chilies, dried shrimp"}),
         knex('dishes').insert({name: 'Thai-style Tuna Ceviche', preptime: 35, img_url: 'https://cincinletseat.files.wordpress.com/2011/05/img_6250.jpg', price: 14, description: 'served on crackers, toasted rice powder, lemongrass, kaffir lime leaf'}),
-        knex('dishes').insert({name: 'Tom Gati of Coconut and Chicken Soup', preptime: 35, img_url: 'http://i.imgsafe.org/3b0b4b5b33.jpg', price: 19, description: 'galangal, lemongrass, young coconut, variety of mushrooms, chili jam'}),
+        knex('dishes').insert({name: 'Tom Gati of Coconut and Chicken Soup', preptime: 35, img_url: 'http://3.bp.blogspot.com/_1T9YXyM1Bfw/SzZ8ggrkaMI/AAAAAAAAFQ4/z0jzpHIme7A/s640/tomkha-gai-version.jpg', price: 19, description: 'galangal, lemongrass, young coconut, variety of mushrooms, chili jam'}),
       ]);
       return insertDataPromise;
     });
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const knex        = require('knex')(knexConfig[ENV]);
 const knexLogger  = require('knex-logger');
 const morgan      = require('morgan');
 const orderRoutes = require('./routes/orders');
-const sass        = require('node-sass-middleware');
+// const sass        = require('node-sass-middleware');
 
 const app         = express();
 
@@ -26,12 +26,12 @@ app.use(knexLogger(knex));
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/styles', sass({
-  src: __dirname + '/styles',
-  dest: __dirname + '/public/styles',
-  debug: true,
-  outputStyle: 'expanded',
-}));
+// app.use('/styles', sass({
+//   src: __dirname + '/styles',
+//   dest: __dirname + '/public/styles',
+//   debug: true,
+//   outputStyle: 'expanded',
+// }));
 app.use(express.static('public'));
 
 // Mount all resource routes
